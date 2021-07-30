dadcdc21a87618f343418fdc36da4aa9a1c814e0
minj-L <wonder_minji@naver.com>
2021-07-30 17:49:49 +0900

    Paging보강 및 Navigator 추가

--- paginateapp2/src/App.js
+++ paginateapp2/src/App.js
@@ -1,19 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import Posts from './components/Posts';
-import Pagination from './components/Pagination';
-import axios from 'axios';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Posts from "./components/Posts";
+import Pagination from "./components/Pagination";
+import Navigator from "./components/Navigator";
+import axios from "axios";
+import "./App.css";
 
 const App = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage] = useState(10);
+  const [postsPerPage] = useState(8);
 
   useEffect(() => {
     const fetchPosts = async () => {
       setLoading(true);
-      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
       setPosts(res.data);
       setLoading(false);
     };
@@ -21,17 +22,22 @@ const App = () => {
     fetchPosts();
   }, []);
 
-  // Get current posts
+  //Get current posts
   const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+  const indexFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexFirstPost, indexOfLastPost);
+
+  console.log(posts);
 
   // Change page
-  const paginate = pageNumber => setCurrentPage(pageNumber);
+  const paginate = (pageNumber) =>setCurrentPage(pageNumber);
 
   return (
-    <div className='container mt-5'>
-      <h1 className='text-primary mb-3'>My Blog</h1>
+    <div className="container mt-5">
+      {" "}
+      {/* className은 단순히 클래스 명이다 */}
+      <Navigator />
+      <h1 className="text-primary mb-3">My Blog</h1>
       <Posts posts={currentPosts} loading={loading} />
       <Pagination
         postsPerPage={postsPerPage}
