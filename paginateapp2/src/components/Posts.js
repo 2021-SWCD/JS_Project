5b779b16859db2e33bd95b42d14879932f8258d1
minj-L <wonder_minji@naver.com>
2021-07-30 17:50:43 +0900

    코드 정리 후 주석 추가

--- paginateapp2/src/components/Posts.js
+++ paginateapp2/src/components/Posts.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const Posts = ({ posts, loading }) => {
   if (loading) {
@@ -6,9 +6,10 @@ const Posts = ({ posts, loading }) => {
   }
 
   return (
-    <ul className='list-group mb-4'>
-      {posts.map(post => (
-        <li key={post.id} className='list-group-item'>
+    <ul className="list-group mb-4">
+      {/* mb-4: 4(24px)만큼의 marginbottom을 줘라 */}
+      {posts.map((post) => (
+        <li key={post.id} className="list-group-item">
           {post.title}
         </li>
       ))}
