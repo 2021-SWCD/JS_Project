0967ae79236ccfc814c2489b4d3dbab08b3f3c24
minj-L <wonder_minji@naver.com>
2021-08-02 12:36:32 +0900

    App에 구현되어 있던 pagination Page.js로 이동

--- paginateapp2/src/App.js
+++ paginateapp2/src/App.js
@@ -1,51 +1,17 @@
-import React, { useState, useEffect } from "react";
-import Posts from "./components/Posts";
-import Pagination from "./components/Pagination";
-import Navigator from "./components/Navigator";
-import axios from "axios";
+import React from "react";
+import Navigator from "./components/navigator/Navigator";
+import Page from "./components/pagenation/Page";
 import "./App.css";
 
-const App = () => {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage] = useState(8);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      setLoading(true);
-      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
-      setPosts(res.data);
-      setLoading(false);
-    };
-
-    fetchPosts();
-  }, []);
-
-  //Get current posts
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = posts.slice(indexFirstPost, indexOfLastPost);
-
-  console.log(posts);
-
-  // Change page
-  const paginate = (pageNumber) =>setCurrentPage(pageNumber);
+export default function App() {
 
   return (
     <div className="container mt-5">
       {" "}
       {/* className은 단순히 클래스 명이다 */}
-      <Navigator />
       <h1 className="text-primary mb-3">My Blog</h1>
-      <Posts posts={currentPosts} loading={loading} />
-      <Pagination
-        postsPerPage={postsPerPage}
-        totalPosts={posts.length}
-        paginate={paginate}
-      />
+      <Navigator />
+      <Page />
     </div>
   );
 };
-
-export default App;
