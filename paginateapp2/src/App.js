25b9df3c9724b05f5fd9d90bb301b662fb18fc06
minj-L <wonder_minji@naver.com>
2021-08-02 17:15:43 +0900

    App파일 단순화

--- paginateapp2/src/App.js
+++ paginateapp2/src/App.js
@@ -7,9 +7,7 @@ export default function App() {
 
   return (
     <div className="container mt-5">
-      {" "}
       {/* className은 단순히 클래스 명이다 */}
-      <h1 className="text-primary mb-3">My Blog</h1>
       <Navigator />
       <Page />
     </div>
